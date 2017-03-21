3205e2f20fec1e0a141ce80161d0a9d65a69c133
Lukas Sidlauskas <lukas.sidlauskas@gmail.com>
2017-03-21 22:31:47 +0200

    Added project related files to repository

---
+++ m3u-generator.js
@@ -0,0 +1,24 @@
+#! /usr/bin/env node
+
+var fs = require('fs');
+var dirname = require('path').basename(__dirname);
+var writer = require('m3u').extendedWriter();
+var audioExtensions = require('audio-extensions');
+var path = process.cwd();
+
+if(!fs.existsSync(path)) return;
+
+fs.readdirSync(path).forEach(function(file) {
+	var ext = file.split('.')[1];
+
+	if(audioExtensions.indexOf(ext) !== -1) {
+		writer.file(file, 0, file);
+	}
+});
+
+fs.writeFile(dirname + '.m3u', writer.toString(), function(err) {
+	if(err) {
+		return console.log(err);
+	}
+	console.log('Playlist file was successfuly generated!')
+});
