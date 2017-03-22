4e522e359637e2536c607df2934886c2722f80f1
Lukas Sidlauskas <lukas.sidlauskas@gmail.com>
2017-03-22 10:05:33 +0200

    Read directory and write file asynchronously

--- m3u-generator.js
+++ m3u-generator.js
@@ -8,17 +8,23 @@ var dirname = path.match(/([^\/]*)\/*$/)[1];
 
 if(!fs.existsSync(path)) return;
 
-fs.readdirSync(path).forEach(function(file) {
-	var ext = file.split('.')[1];
-
-	if(audioExtensions.indexOf(ext) !== -1) {
-		writer.file(file, 0, file);
-	}
-});
-
-fs.writeFile(dirname + '.m3u', writer.toString(), function(err) {
+fs.readdir(path, function(err, files) {
 	if(err) {
 		return console.log(err);
 	}
-	console.log('Playlist file was successfuly generated!')
+
+	files.forEach(function(file) {
+		var ext = file.split('.')[1];
+		if(audioExtensions.indexOf(ext) !== -1) {
+			writer.file(file, 0, file);
+		}
+	});
+
+	fs.writeFile(dirname + '.m3u', writer.toString(), function(err) {
+		if(err) {
+			return console.log(err);
+		}
+		console.log('Playlist file was successfuly generated!')
+	});
+
 });
