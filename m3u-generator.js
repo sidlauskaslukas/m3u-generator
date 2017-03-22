bd1321d975fc7956ce4c692f352b2780996845a4
Lukas Sidlauskas <lukas.sidlauskas@gmail.com>
2017-03-22 11:27:11 +0200

    Do not create the playlist file if the audio files does not exist. Refactored the code.

--- m3u-generator.js
+++ m3u-generator.js
@@ -4,26 +4,46 @@ var fs = require('fs');
 var writer = require('m3u').extendedWriter();
 var audioExtensions = require('audio-extensions');
 var path = process.cwd();
-var dirname = path.match(/([^\/]*)\/*$/)[1];
-
-if(!fs.existsSync(path)) return;
 
 fs.readdir(path, function(err, files) {
 	if(err) {
-		return console.log(err);
+		if(err.code === 'ENOENT') {
+			console.log('This folder does not exist.');
+			return;
+		} else {
+			throw err;
+		}
+	} else {
+		readFiles(files, function() {
+			createPlaylistFile();
+		}, function() {
+			console.log('There are no audio files in this folder!');
+		});
 	}
+});
+
+function readFiles(files, onSuccess, onError) {
+	var writeFile = false;
 
 	files.forEach(function(file) {
 		var ext = file.split('.')[1];
+
 		if(audioExtensions.indexOf(ext) !== -1) {
+			writeFile = true;
 			writer.file(file, 0, file);
 		}
 	});
 
-	fs.writeFile(dirname + '.m3u', writer.toString(), function(err) {
+	writeFile ? onSuccess() : onError();
+}
+
+function createPlaylistFile() {
+	var playlistName = path.match(/([^\/]*)\/*$/)[1];
+
+	fs.writeFile(playlistName + '.m3u', writer.toString(), function(err) {
 		if(err) {
 			return console.log(err);
 		}
 		console.log('Playlist file was successfuly generated!')
 	});
-});
+}
